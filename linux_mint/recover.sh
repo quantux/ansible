74f3c4f7206775812fd55c114a2892e5f27af486
Matheus Faria <mths.faria@outlook.com>
2025-10-10 17:10:46 -0300

    Initial commit.

---
+++ linux_mint/recover.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+# Global
+DOCKER_COMPOSE_PATH="$USER_HOME/.scripts/docker-apps/docker-compose.yml"
+RESTIC_REPO="/media/restic/restic_notebook_repo"
+
+# Testa se o restic está instalado
+if command -v restic >/dev/null 2>&1; then
+    echo "✅ Restic está instalado."
+    restic version
+else
+    echo "❌ Restic não está instalado."
+    exit 1
+fi
+
+# Restaurar diretamente para a home
+show_message "Restaurando backup Restic diretamente para $USER_HOME..."
+restic -r "$RESTIC_REPO" restore latest --target / --tag mths --tag linux_mint
+
+# Set mirrors
+show_message "Atualizando mirrors"
+cp /etc/apt/sources.list.d/official-package-repositories.list /etc/apt/sources.list.d/official-package-repositories.list.bkp
+sed -i "s/wilma/$LINUXMINT_CODENAME/g; s/noble/$UBUNTU_CODENAME/g" /etc/apt/sources.list.d/official-package-repositories.list
+
+# 32bits packages
+show_message "Habilitando pacotes de 32 bits"
+dpkg --add-architecture i386
+
+# Update
+show_message "Atualizando repositórios"
+apt-get update
+
+# Upgrade
+show_message "Atualizando pacotes"
+apt-get upgrade -y
+
+# Install apt-get packages
+show_message "Instalando pacotes"
+apt-get install -y $(cat pacotes_apt.txt)
+
+# Nvidia Container Toolkit repository
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+# Instalando libdvd-pkg
+show_message "Instalando libdvd-pkg"
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y install libdvd-pkg
+dpkg-reconfigure -f noninteractive libdvd-pkg
+
+# Install ttf-mscorefonts-installer
+show_message "Instalando ttf-mscorefonts-installer"
+echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula	boolean	true" | debconf-set-selections
+echo "ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note" | debconf-set-selections
+apt-get install -y ttf-mscorefonts-installer
+
+# Install - Roboto and Noto Sans Fonts
+show_message "Instalando fontes Roboto e Noto Sans"
+wget "https://fonts.google.com/download?family=Roboto" -O /tmp/roboto.zip
+wget "https://fonts.google.com/download?family=Noto Sans" -O /tmp/noto_sans.zip
+unzip /tmp/roboto.zip -d /usr/share/fonts/
+unzip /tmp/noto_sans.zip -d /usr/share/fonts/
+
+# Install NerdFront Firacode
+show_message "Instalando fontes NerdFont Firacode"
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/FiraCode.zip" -O /tmp/firacode.zip
+unzip /tmp/firacode.zip -d /usr/share/fonts/
+
+# Install - Adapta Nokto theme
+show_message "Instalando Adapta Nokto"
+tar -xf ./assets/themes/Adapta-Nokto.tar.xz -C /usr/share/themes
+
+# Install Flat Remix theme
+show_message "Instalando Flat Remix theme"
+tar -xf ./assets/themes/Flat-Remix-GTK-Blue-Darkest-Solid-NoBorder.tar.xz -C /usr/share/themes
+
+# La-Capitaine Icons
+show_message "Instalando ícones La-Capitaine"
+tar -xf ./assets/icons/la-capitaine.tar.xz -C /usr/share/icons/
+
+# WPS Office Fonts
+show_message "Instalando fontes para o WPS Office"
+git clone https://github.com/udoyen/wps-fonts.git /tmp/wps-fonts
+mv /tmp/wps-fonts/wps /usr/share/fonts/
+
+# Load dconf file
+show_message "Carregando configurações do dconf"
+user_do "DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/${USER_UID}/bus' dconf load / < $USER_HOME/.dconf/dconf"
+
+# Update tldr
+user_do "tldr --update"
+
+# Instalando virtualbox-guest-x11
+show_message "Instalando virtualbox-guest-x11"
+yes Y | apt-get install -y virtualbox-guest-x11
+
+# Add user to vbox group
+usermod -aG vboxusers $USER_NAME
+
+# Install flatpak packages
+show_message "Instalando pacotes flatpak"
+flatpak install -y --noninteractive flathub $(cat pacotes_flatpak.txt)
+
+# Update flatpak
+show_message "Atualizando pacotes flatpak"
+flatpak update -y
+
+# Install Anydesk
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc
+chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc
+echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null
+apt update
+apt install -y anydesk
+
+# Install VSCode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] \
+https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+apt update
+apt install -y code
+
+# Install nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+rm -rf /opt/nvim
+tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# Install Teamviewer
+show_message "Instalando TeamViewer"
+wget "https://download.teamviewer.com/download/linux/teamviewer_amd64.deb" -O /tmp/teamviewer.deb
+dpkg -i /tmp/teamviewer.deb
+apt install -fy
+
+# Install oh-my-posh
+show_message "Instalando oh-my-posh"
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+chmod +x /usr/local/bin/oh-my-posh
+
+# Set qBitTorrent as default magnet link app
+xdg-mime default org.qbittorrent.qBittorrent.desktop x-scheme-handler/magnet
+
+# Allow games run in fullscreen mode
+echo "SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS=0" >> /etc/environment
+
+# Install Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
+# Install Docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker "${SUDO_USER:-$USER}"
+
+# Start containers
+docker compose -f "$DOCKER_COMPOSE_PATH" up -d
+
+# Define zsh como shell padrão
+show_message "Definir zsh como shell padrão"
+chsh -s $(which zsh) $USER_NAME
+user_do "chsh -s $(which zsh)"
+
+# Reiniciar
+show_message ""
+while true; do
+    read -p "Finalizado! Deseja reiniciar? (y/n): " yn
+    case $yn in
+        [Yy]* ) reboot; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
